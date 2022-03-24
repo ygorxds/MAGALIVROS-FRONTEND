02594167639d80bfe1e28c931a81c0940452873e
ygorpsa <ygordesa2010@hotmail.com>
2022-03-24 13:07:03 -0300

    feat: Tela de em breve

---
+++ CADASTRO DE LIVROS/script.js
@@ -0,0 +1,9 @@
+function criar() {
+  const password = document.getElementById('passoword')
+  const confirm = document.getElementById('confirm')
+  const res = document.getElementById('res')
+
+  alert('Nós não temos responsábilidade se você está cadastrando um livro pirata na sua biblioteca, caso esteja fazendo isso a pica é sua!')
+
+  res.innerHTML = "Livro Cadastrado"
+}
